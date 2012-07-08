893ef68bd8502b618a58e025b8f73267a45a1b30
Alex Nichol <alexnichol@comcast.net>
2012-07-08 18:23:57 -0400

    Initial Commit

---
+++ PBar/ANAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ANAppDelegate.h
+//  PBar
+//
+//  Created by Alex Nichol on 7/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ANAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
